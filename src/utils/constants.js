15c53934c979b52fdbe2e127295eb106f94d837d
shams261 <shamsshoaib261@gmail.com>
2025-05-26 18:37:57 -0400

    constant updated for dev and sandbox

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
-//  export const BASE_URL = "http://localhost:3000";
+// for sandbox environment
+export const BASE_URL = "http://localhost:3000";
 
-export const BASE_URL = "/api";
+//for development, you can use the following line
+// export const BASE_URL = "/api";
